ad7b3e66d9c88d59bb338f64e2b88cf6bfdc82e2
Charumathi S <charumathis2006@gmail.com>
2026-04-20 00:01:40 +0530

    fix dataloader detection

--- src/test/java/com/academic/backend/DataLoader.java
+++ src/test/java/com/academic/backend/DataLoader.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Component
 public class DataLoader {
 
     @Bean
